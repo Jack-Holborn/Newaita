370376227305ff70ca6701d7c4d45727de95615d
Jack Holborn <jack@winbox.homelinux.lan>
2021-05-31 19:23:07 +0200

    encore 1

--- Newaita/FV.sh
+++ Newaita/FV.sh
@@ -18,6 +18,7 @@ echo "choose your folders variant
 7) Mint
 8) Ubuntu
 9) Default
+<<<<<<< HEAD
 10) Quit"
 
 read n
@@ -44,4 +45,57 @@ ln -s ./.places-ubuntu ./places;;
 ln -s ./.places-default ./places;;
 10)break;;
 *)invalid option;;
+=======
+10) Indigo
+11) Red
+12) Yellow
+13) Cyan
+14) b-Bluegray
+15) b-Brown
+16) b-Purple
+17) b-Violette
+18) b-Yellow
+19) Quit
+>" n
+
+case $n in
+	1) rm -f ./places
+		ln -vs ./.places-aqua ./places;;
+	2) rm -f ./places
+		ln -vs ./.places-blue ./places;;
+	3) rm -f ./places
+		ln -vs ./.places-bluegray ./places;;
+	4) rm -f ./places
+		ln -vs ./.places-darknight ./places;;
+	5) rm -f ./places
+		ln -vs ./.places-gray ./places;;
+	6) rm -f ./places
+		ln -vs ./.places-manjaro ./places;;
+	7) rm -f ./places
+		ln -vs ./.places-mint ./places;;
+	8) rm -f ./places
+		ln -vs ./.places-ubuntu ./places;;
+	9) rm -f ./places
+		ln -vs ./.places-default ./places;;
+	10)rm -f ./places
+		ln -vs ./.places-indigo ./places;;
+	11)rm -f ./places
+		ln -vs ./.places-red ./places;;
+	12)rm -f ./places
+		ln -vs ./.places-yellow ./places;;
+	13)rm -f ./places
+		ln -vs ./.places-cyan ./places;;	
+	14) rm -f ./places
+		ln -vs .places-b-bluegray ./places;;
+	15) rm -f ./places
+		ln -vs .places-b-brown ./places;;
+	16) rm -f ./places
+		ln -vs .places-b-purple ./places;;
+	17) rm -f ./places
+		ln -vs .places-b-violette ./places;;
+	18) rm -f ./places
+		ln -vs .places-b-yellow ./places;;
+	19) exit;;
+	*) echo "invalid option";;
+>>>>>>> master
 esac
