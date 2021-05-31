088025107fd5624bdd761e3f0ea874cd998aeece
Jack Holborn <jack@winbox.homelinux.lan>
2021-05-31 19:41:34 +0200

    tentative de merge 1

--- Newaita/FV.sh
+++ Newaita/FV.sh
@@ -1,13 +1,14 @@
+ 
 #!/bin/bash
-#
+
 if ! grep -q "Newaita" ./README.md; then
-	echo "Error: You are not inside Newaita directory"
-	echo "Do:"
-	echo " # cd /path/to/Newaita"
-	exit 1
+echo "Error: You are not inside Newaita directory"
+echo "Do:"
+echo " # cd /path/to/Newaita"
+exit 1
 fi
 
-read -p "choose your folders variant
+echo "choose your folders variant
 1) Aqua
 2) Blue
 3) Bluegray
@@ -17,6 +18,34 @@ read -p "choose your folders variant
 7) Mint
 8) Ubuntu
 9) Default
+<<<<<<< HEAD
+10) Quit"
+
+read n
+
+case $n in
+
+1)rm -rf ./places
+ln -s ./.places-aqua ./places;;
+2) rm -rf ./places
+ln -s ./.places-blue ./places;;
+3)rm -rf ./places
+ln -s ./.places-bluegray ./places;;
+4)rm -rf ./places
+ln -s ./.places-darknight ./places;;
+5)rm -rf ./places
+ln -s ./.places-gray ./places;;
+6)rm -rf ./places
+ln -s ./.places-manjaro ./places;;
+7)rm -rf ./places
+ln -s ./.places-mint ./places;;
+8)rm -rf ./places
+ln -s ./.places-ubuntu ./places;;
+9)rm -rf ./places
+ln -s ./.places-default ./places;;
+10)break;;
+*)invalid option;;
+=======
 10) Indigo
 11) Red
 12) Yellow
@@ -68,4 +97,5 @@ case $n in
 		ln -vs .places-b-yellow ./places;;
 	19) exit;;
 	*) echo "invalid option";;
+>>>>>>> master
 esac
