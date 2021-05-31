b2ac623473caed40c639984821d991850178a98c
Jack Holborn <jack@winbox.homelinux.lan>
2021-05-31 19:12:06 +0200

    merge cbernix repo into jack
    bof1

--- Newaita-dark/FV.sh
+++ Newaita-dark/FV.sh
@@ -1,13 +1,14 @@
+ 
 #!/bin/bash
-#
+
 if ! grep -q "Newaita" ./README.md; then
-	echo "Error: You are not inside Newaita directory"
-	echo "Do:"
-	echo " # cd /path/to/Newaita"
-	exit 1
+echo "Error: You are not inside Newaita directory"
+echo "Do:"
+echo " # cd /path/to/Newaita"
+exit 1
 fi
 
-read -p "choose your folders variant
+echo "choose your folders variant
 1) Aqua
 2) Blue
 3) Bluegray
@@ -17,40 +18,30 @@ read -p "choose your folders variant
 7) Mint
 8) Ubuntu
 9) Default
-10) Indigo
-11) Red
-12) Yellow
-13) Cyan
-14) Quit
->" n
+10) Quit"
+
+read n
 
 case $n in
-	1) rm -f ./places
-		ln -s ./.places-aqua ./places;;
-	2) rm -f ./places
-		ln -s ./.places-blue ./places;;
-	3) rm -f ./places
-		ln -s ./.places-bluegray ./places;;
-	4) rm -f ./places
-		ln -s ./.places-darknight ./places;;
-	5) rm -f ./places
-		ln -s ./.places-gray ./places;;
-	6) rm -f ./places
-		ln -s ./.places-manjaro ./places;;
-	7) rm -f ./places
-		ln -s ./.places-mint ./places;;
-	8) rm -f ./places
-		ln -s ./.places-ubuntu ./places;;
-	9) rm -f ./places
-		ln -s ./.places-default ./places;;
-	10)rm -f ./places
-		ln -s ./.places-indigo ./places;;
-	11)rm -f ./places
-		ln -s ./.places-red ./places;;
-	12)rm -f ./places
-		ln -s ./.places-yellow ./places;;
-	13)rm -f ./places
-		ln -s ./.places-cyan ./places;;	
-	14) exit;;
-	*) echo "invalid option";;
+
+1)rm -rf ./places
+ln -s ./.places-aqua ./places;;
+2) rm -rf ./places
+ln -s ./.places-blue ./places;;
+3)rm -rf ./places
+ln -s ./.places-bluegray ./places;;
+4)rm -rf ./places
+ln -s ./.places-darknight ./places;;
+5)rm -rf ./places
+ln -s ./.places-gray ./places;;
+6)rm -rf ./places
+ln -s ./.places-manjaro ./places;;
+7)rm -rf ./places
+ln -s ./.places-mint ./places;;
+8)rm -rf ./places
+ln -s ./.places-ubuntu ./places;;
+9)rm -rf ./places
+ln -s ./.places-default ./places;;
+10)break;;
+*)invalid option;;
 esac
--- Newaita/PV.sh
+++ Newaita/PV.sh
@@ -1,23 +1,25 @@
  
 #!/bin/bash
 if ! grep -q "Newaita" ./README.md; then
-	echo "Error: You are not inside Newaita directory"
-	echo "Do:"
-	echo " # cd /path/to/Newaita"
-	exit 1
+echo "Error: You are not inside Newaita directory"
+echo "Do:"
+echo " # cd /path/to/Newaita"
+exit 1
 fi
 
-read -p "choose your variant
+echo "choose your variant
 1) Dark panel
 2) Light panel
-3) Quit
->" n
+3) Quit"
+
+read n
 
 case $n in
-	1) rm -f ./panel
-		ln -s ./.DP ./panel;;
-	2) rm -f ./panel
-		ln -s ./.LP ./panel;;
-	3) exit;;
-	*)invalid option;;
+
+1) rm -rf ./panel
+ln -s ./.DP ./panel;;
+2)rm -rf ./panel
+ln -s ./.LP ./panel;;
+3) break;;
+*)invalid option;;
 esac
